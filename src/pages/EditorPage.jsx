920c52229a310e963884049e7c14e373efd4f4dd
Aditya Aupta <guptaaditya854@gmail.com>
2022-05-30 15:26:54 +0530

    server

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef,useEffect } from 'react';
 import Client from '../components/Client';
 import Editor from '../components/Editor';
+import Codeeditor from "../Assets/codeeditor.png"
+import { initSocket } from '../socket';
+import ACTIONS from '../Actions';
+import { useLocation } from 'react-router-dom';
+
 const EditorPage =()=>
 {
-    const [clients,setClients]=useState([{socketId: 1, username: 'Vipul'},
-    {socketId: 2, username: 'Aditya'}
-]);
-return (
+    const socketRef = useRef(null);
+    const location = useLocation();
+
+    useEffect(() => {
+       const init = async() =>{
+           socketRef.current = await initSocket();
+        //    socketRef.current.emit(ACTIONS.JOIN,{
+        //        roomId,
+        //        username:location.state?.username,
+        //    });
+       };
+       init();
+    }, []);
+
+    const [clients,setClients]=useState([{socketId: 1, username: 'Vipul'}
+    ]);
+    return (
     <div className='mainWrap'>
     <div className='aside'>
     <div className='aside Inner'>
         <div className='logo'>
-            <img className='logoImage' src="{codeeditor.png}" alt='logo'/>
+            <img className='logoImage' src={Codeeditor} alt='logo'/>
         </div>
         <h3>Connected</h3>
         <div className='clientsList'>
@@ -19,8 +37,9 @@ return (
             clients.map((client=><Client key={client.socketId} username={client.username}/>))
         }
 
-</div>
-        </div>
+ </div>
+        </div >
+        
         <button className='btn copyBtn'>Copy Room ID</button>
         <button className='btn leaveBtn'>Leave</button>
     </div>
